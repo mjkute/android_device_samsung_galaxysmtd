2bccebcf34dd4452589783bfb9c9c236a1414b86
mjkute <tomjhao@gmail.com>
2013-10-04 19:20:12 +0700

    Update crom.mk

--- crom.mk
+++ crom.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, vendor/crom/config/common_gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := galaxysmtd
+PRODUCT_RELEASE_NAME := captivatemtd
 
 # languages
 PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
@@ -30,10 +30,10 @@ $(call inherit-product, vendor/crom/config/common_phone.mk)
 $(call inherit-product, device/samsung/galaxysmtd/full_galaxysmtd.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := galaxysmtd
-PRODUCT_NAME := crom_galaxysmtd
+PRODUCT_DEVICE := captivate
+PRODUCT_NAME := crom_captivatemtd
 PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := GT-I9000
+PRODUCT_MODEL := SGH-i897
 
 # Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9000 TARGET_DEVICE=GT-I9000 BUILD_FINGERPRINT=samsung/GT-I9000/GT-I9000:2.3.5/GINGERBREAD/XXJVT:user/release-keys PRIVATE_BUILD_DESC="GT-I9000-user 2.3.5 GINGERBREAD XXJVT release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-i897 TARGET_DEVICE=SGH-i897 BUILD_FINGERPRINT=samsung/SGH-i897/SGH-i897:2.3.5/GINGERBREAD/XXJVT:user/release-keys PRIVATE_BUILD_DESC="SGH-i897-user 2.3.5 GINGERBREAD XXJVT release-keys"
